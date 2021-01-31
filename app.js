7ec39a8b9d7fb07221ecf6fb07e6a8fa0c376817
Todd Riggleman <toddriggleman@MacBook-Pro.home>
2021-01-31 08:11:05 -0500

    basically finished all the api stuff

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const app = express();
 const https = require('https');
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=c5aa0e14eede866dbebaaccdeca42c4d&units=imperial"
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");   
+});
+
+app.post("/", (req, res) => {
+
+    var city = req.body.location;
+    //console.log(city);
+
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c5aa0e14eede866dbebaaccdeca42c4d&units=imperial`;
 
     https.get(url, (response) => {
-        console.log(response);
+        response.on('data', (data) => {
+            //alll the json weather data
+            const weather = JSON.parse(data);
+
+            const img = weather.weather[0].icon;
+            const imgURL = " http://openweathermap.org/img/wn/"+img+"@2x.png"
+            const weatherDegress = weather.main.temp;
+            const weatherDiscription = weather.weather[0].description;
+            const weatherLocation = weather.name;
+            res.write("<h1>The weather in "+ weatherLocation +" is " + weatherDegress + " degress</h1>");
+            res.write("<p>It is " + weatherDiscription + "</p>");
+            res.write("<img src= "+ imgURL + ">");
+            res.send();
+        })
     })
+
 });
 
 app.listen(8080, () => {
